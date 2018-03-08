8f702395606d6994ba5c4795d4e13e06860dce26
TonyPbA <t.price@bham.ac.uk>
2018-03-08 01:17:03 +0000

    Fixing mentions of tracker, dynamite and RT wq

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -22,18 +22,10 @@ class EventAction : public G4UserEventAction
     void EndOfEventAction(const G4Event*);
     void AddStep(const G4Step* step);
     void AddAbsorberStep(const G4Step* step);
-		void ResetDynamite();
-    void AddDynamiteStep(const G4Step* step);
-		void AddSensorStep(const G4Step* step);
-		void EndOfEventDynamiteAction();
-		void AddAirGapStep(const G4Step* step);
-		void AddDoubleDynamiteStep(const G4Step* step);
-		void AddTrackerStep(const G4Step* step);
-		void AddRangeTelescopeStep(const G4Step* step);
-		void AddValidationStep(const G4Step* step);
-		G4ThreeVector CalculateDoubleDynamiteDisplacement();
-		G4ThreeVector CalculateDoubleDynamiteDeflection();
-		
+	void AddSensorStep(const G4Step* step);
+	void AddAirGapStep(const G4Step* step);
+	void AddValidationStep(const G4Step* step);
+	
 		//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 		void AddBeamPipePosition(G4ThreeVector pos){ beampipe_pos_int.push_back(pos);};
 		void ProtonInAbsorber(G4bool b){ pInAbsorber = b; };
@@ -45,8 +37,6 @@ class EventAction : public G4UserEventAction
 		G4double ConvertEDeptoElectrons(G4double);
 		G4int			GetEventID();
 		G4int			GetRndmSeed();
-		G4int     GetProcessType(G4String processName);
-    
   private:
   	HistoManager* histoManager;
   	DetectorConstruction* detector;
@@ -56,8 +46,7 @@ class EventAction : public G4UserEventAction
 		
 		std::vector<G4ThreeVector> beampipe_pos_int;
 		G4bool pInAbsorber;
-  
-		G4double totalEdepAirGapPerEvent;
+	G4double totalEdepAirGapPerEvent;
   	G4double             totalEDep;
   	G4double             totalNIEL;
 		
@@ -71,28 +60,6 @@ class EventAction : public G4UserEventAction
   	G4double             AbsorberIncidentEnergy;
   	G4double             AbsorberExitingEnergy;
   	
-  	G4double             dynamiteEDep[6];
-  	G4double             dynamiteNIEL[6];
-  	G4int                nDynamiteIncidentSteps;
-		G4int dynamiteSteps;
-		
-		const G4DynamicParticle*   doubledynamiteProton[2];
-		G4ThreeVector        doubleDynamiteProtonMomentum[2];
-		std::vector<const G4Step*> doubledynamiteSteps[2];
-		G4ThreeVector        doubleDynamiteIncidentPosition[2];
-		G4ThreeVector        doubleDynamiteIncidentMomentum[2];
-		G4double             doubleDynamiteEnergyDepositedInEpi[2];
-		bool                 doubleDynamiteIsAProton[2];
-		
-		
-		G4double							MeVtoElectron;
-		std::vector<const G4Step*> trackerSteps[12];
-		G4double							trackerEDep[12];
-		G4double							trackerNIEL[12];
-		
-		std::vector<const G4Step*> rangetelSteps[3][30];
-		G4double							rangetelEDep[3][30]; // total, epi, sub
-		G4double							rangetelNIEL[3][30]; // total, epi, sub
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
